5d41f34beb54aa0bffab74a687420fa40e7626da
Samuel <samuelftr@gmail.com>
2019-04-11 12:23:22 -0300

    terceiro commit

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192   //Numero de endereços
+        D=A     // D recebe 8192
+        @count  // A fica com valor de @count, que é 16
+
+        M=D     // coloca 8192 no endereço 16
+(LOOP)
+        @temp // a fica com valor de temp, que é 17
+        M=0     // ram[17]= temp = 0
+(VERIFICACAO)
+        @KBD //espaço na memoria dedicado à leitura do teclado, que é o end. 24576
+        D=M 
+        @PERMOUADDBRANCO //permanece (ou adiciona) branco na tela
+        D;JEQ   // dá um jump pra parte de preencher com branco se KBD for 0 (Ou seja nao leu nada do teclado), caso contrario
+        //vai pro label responsavel pela parte de preencher com preto
+(PREENPRETO)
+        @temp
+        D=M
+        @SCREEN
+        A=A+D   
+        M=-1    // Preenche o campo com -1, significando preto
+        @PTFINAL
+        0;JMP   // jump pra parte final (aqui já passou a parte de preenchimento, basta apenas agora avançar a coluna e repetir o processo)
+(PERMOUADDBRANCO)
+        @temp
+        D=M
+        @SCREEN
+        A=A+D   
+        M=0     // Preenche o campo com 0, significando branco
+(PTFINAL)   
+        @temp
+        D=M+1  
+        M=M+1
+        //D=D+1 
+        //M=M+1 
+        //Pularia 1 casa
+        //Com esses comandos foi aumentado o índice e agora passará a preencher (ou não) na próxima coluna
+        @count
+        D=D-M
+        @LOOP
+        D;JEQ   
+        @VERIFICACAO
+        0;JMP   //Pula pra parte de verificação, repetindo o processo
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+     @2 //RAM[2]
+     M=0 //Colocando 0 no resultado final, RAM[2] = R2
+
+//Verificando se o 1o num é 0:
+    @0
+    D=M //Coloca em D o valor de R0 
+    @FIM
+    D;JEQ //Obviamente, se R0 for 0, R2 será 0 e o processo acaba.
+
+//Verificando se o 2o num. é 0:
+    @1
+    D=M
+    @FIM
+    D;JEQ	//Se R1 for 0, R2 será 0 e o processo acaba.
+
+//Armazenando o valor de R0 no endereço 3
+    @0
+    D=M	// Põe em D o valor de R0
+    @3	
+    M=D	//Põe D, que é R0, em RAM[3], que será quantas vezes o processo será feito
+        //Ou seja, quantas vezes R1 será somado com ele mesmo
+
+//Loop de soma até que RAM[3] seja 0, ou seja, até que o numero tenha sido somado com ele mesmo R0 vezes
+
+(LOOP)
+    @1	
+    D=M	//D tem o valor de R1
+
+    @2	
+    M=D+M //Soma o valor de R1 + o valor de R2
+
+    @3	
+    M=M-1 //Diminui o valor de RAM[3] (que nada mais é do que a qtd de vezes que o num. será somado com ele mesmo)
+         //até que chegue 0
+
+    D=M	//D = RAM[3]
+    @LOOP	//
+    D;JGT	//Volta pro LOOP se D, que é RAM[3], for>0
+
+(FIM)
+    @FIM
+    //@2 Melhor colocar no início pois evita que o loop final fique colocando 0 em RAM[2]
+    //M=0
+    0;JMP //LOOP interminável
